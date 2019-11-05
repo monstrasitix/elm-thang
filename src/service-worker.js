7fbf283a00298ffc0169c40219e0a38411ce3d9d
dmitrijssavostijanovs <dmitrijs@groupmail.io>
2019-11-05 00:27:34 +0000

    At least images are caching :D

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,18 +5,18 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest);
 // workbox.core.clientsClaim();
 
 
+workbox.routing.registerRoute(
+    /https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/font-awesome\/5\.11\.2\/css\/all\.css/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'font-awesome-cache',
+    }),
+);
 
-// workbox.precaching.precacheAndRoute(
-//     [
-//         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css'
-//     ],
-//     new workbox.strategies.StaleWhileRevalidate(),
-// );
 
-// workbox.precaching.registerRoute(
-//     /\.(?:png|gif|jpg|svg)$/,
-//     new workbox.strategies.CacheFirst({
-//         cacheName: 'images-cache',
-//     }),
-// );
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|svg)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'images-cache',
+    }),
+);
 
